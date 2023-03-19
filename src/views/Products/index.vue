dd0fce00424c2f44671961fcfe20d68cd48fd3fb
k.berdelis <k.berdelis@deepsea.ai>
2023-03-19 13:04:44 +0200

    repos/dir

--- src/views/Products/index.vue
+++ src/views/Products/index.vue
@@ -2,6 +2,15 @@
 <template>
   <div id="products">
     <h1 class="subheading grey--text router-header-name">Repos</h1>
-    <v-container class="router-container"> content </v-container>
+    <v-container class="router-container"> 
+      <v-expansion-panels>
+      <v-expansion-panel
+        v-for="repo in repositories"
+        :key="repo.id"
+        :title="repo.name"
+        :text="repo.description"
+      ></v-expansion-panel>
+    </v-expansion-panels>
+    </v-container>
   </div>
 </template>
