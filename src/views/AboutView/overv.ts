faaa3b7131b0513e7d9a4356fb527a66bd3d7f08
k.berdelis <k.berdelis@deepsea.ai>
2023-03-24 00:07:26 +0200

    chart.js with oopApi

--- src/views/AboutView/overv.ts
+++ src/views/AboutView/overv.ts
@@ -1,54 +1,57 @@
 import { defineComponent } from "vue";
 import { Bar } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
 import GithubApi from "@/utils/api/githubAPI";
 
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-);
+ChartJS.register( Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale );
 
 export default defineComponent({
   name: "BarChart",
   components: { Bar },
   data() {
     return {
+      githubCommits: [] as any,
+      githubRepos: [] as any,
       githubRepositories: [] as any,
-      chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
-      },
+      newObj: new GithubApi(),
       chartOptions: {
         responsive: true,
+        animation: false,
+
       },
     };
   },
-  mounted () {
+  computed: {
+    // a computed getter
+    chartData() {
+      this.githubRepositories.map(async (repo: any) => {
+        this.githubRepos.push(repo.name);
+        const dta = await this.newObj.getCommits(repo.name);
+        this.githubCommits.push(dta.length);
+
+        return dta.length;
+      });
+      return {
+        labels: this.githubRepos.slice(0, 5),
+        datasets: [{ data: this.githubCommits.slice(0, 5) }],
+      };
+    },
+  },
+  mounted() {
     this.getObj();
   },
   methods: {
     async getObj() {
-      const obj = new GithubApi();
-      this.githubRepositories = await obj.repositories.then((dta) => { 
-        return dta
-       });
-      const commits = await obj.getCommits(this.githubRepositories[0].name).then((commit) => { return commit});
-      // console.log("this.githubRepositories: ",this.githubRepositories[0].name);
-      // console.log("obj: ",Object.entries(this.githubRepositories));
-      // console.log("commits: ",commits.length);
+      this.githubRepositories = await this.newObj.repositories.then((dta) => {
+        return dta;
+      });
+      // this.githubRepositories.map(async (repo: any) => {
+      //   this.githubRepos.push(repo.name);
+      //   const dta = await obj.getCommits(repo.name);
+      //   this.githubCommits.push(dta.length);
 
-    }
-  }
+      //   return dta.length;
+      // });
+    },
+  },
 });
